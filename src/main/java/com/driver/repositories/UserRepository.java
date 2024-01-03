378bfd28bf3f0641fc7ed6337af798a6ef712f72
Shashwata Das <shashwatadas802@gmail.com>
2024-01-03 22:38:52 +0530

    Commit 13

--- src/main/java/com/driver/repositories/UserRepository.java
+++ src/main/java/com/driver/repositories/UserRepository.java
@@ -1,7 +1,5 @@
 package com.driver.repositories;
-import com.driver.models.Blog;
 import com.driver.models.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 @Repository
