3049adfa7cb1c9392d386a5ecb62c5bbd41664bc
Shashwata Das <shashwatadas802@gmail.com>
2024-01-03 22:10:27 +0530

    fourth commit

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Blog {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
 
     private int id;
     private String title;
