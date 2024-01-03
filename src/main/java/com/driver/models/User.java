3117b02e8baf5481c77dd7b7147a3494ad1e3d0a
Shashwata Das <shashwatadas802@gmail.com>
2024-01-03 22:00:31 +0530

    second commit

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -1,20 +1,28 @@
-package com.driver.models;
+package  com.driver.models;
+
+
+import org.springframework.boot.test.autoconfigure.data.cassandra.DataCassandraTest;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
 @Entity
-public class User {
+@Table(name = "user_info")
+public  class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private String username;
-    private String password;
-    private String firstName;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+
+    private  String username;
+    private  String password;
+    private  String firstName;
     private String lastName;
-    @OneToMany(mappedBy = "user")
-    private List < Blog > blogList = new ArrayList < > ();
-    public User() {}
-    public User(int id, String username, String password, String firstName, String lastName, List < Blog > blogList) {
+
+    @OneToMany(mappedBy = "user" ,cascade =CascadeType.ALL)
+    private List<Blog> blogList = new ArrayList<>();
+
+    public User(int id, String username, String password, String firstName, String lastName, List<Blog> blogList) {
         this.id = id;
         this.username = username;
         this.password = password;
@@ -22,40 +30,56 @@ public class User {
         this.lastName = lastName;
         this.blogList = blogList;
     }
+
+    public User(){
+
+    }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public List < Blog > getBlogList() {
+
+    public List<Blog> getBlogList() {
         return blogList;
     }
-    public void setBlogList(List < Blog > blogList) {
+
+    public void setBlogList(List<Blog> blogList) {
         this.blogList = blogList;
     }
 }
--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,26 +1,35 @@
 package com.driver.services;
+
 import com.driver.models.*;
 import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
+
+
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository3;
+
     public User createUser(String username, String password) {
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
         user.setFirstName("test");
         user.setLastName("test");
-        userRepository3.save(user);
-        return user;
+
+        return userRepository3.save(user);
+
     }
+
     public void deleteUser(int userId) {
         userRepository3.deleteById(userId);
+
     }
+
     public User updateUser(Integer id, String password) {
         User user = userRepository3.findById(id).get();
         user.setPassword(password);
